da290870c761a43d03c63bc371a7720d4b985df7
Jeroen Plug <1918054+jeroen-plug@users.noreply.github.com>
2025-12-09 00:47:06 +0100

    fix(2025): Learned about disjoint-set, updated day 8

--- 2025/src/bin/8.rs
+++ 2025/src/bin/8.rs
@@ -1,21 +1,68 @@
 aoc::solution!(8, "");
 
-use std::collections::HashMap;
-
-#[derive(Debug, Clone, Copy, Default, Eq, Hash, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 struct Point {
     x: u64,
     y: u64,
     z: u64,
 }
 
-pub fn part_1(input: &str) -> Option<u64> {
+struct DisjointSet {
+    parent: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        DisjointSet {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    fn union(&mut self, a: usize, b: usize) {
+        let mut a = self.find(a);
+        let mut b = self.find(b);
+
+        if a == b {
+            return;
+        }
+
+        if self.size[a] < self.size[b] {
+            std::mem::swap(&mut a, &mut b);
+        }
+
+        self.parent[b] = a;
+        self.size[a] += self.size[b];
+    }
+
+    fn find_size(&mut self, x: usize) -> usize {
+        let x = self.find(x);
+        self.size[x]
+    }
+}
+
+pub fn part_1(input: &str) -> Option<usize> {
     part_1_inner(input, 1000)
 }
 
-fn part_1_inner(input: &str, max_iterations: usize) -> Option<u64> {
-    let (mut sizes, _, _) = solve(input, Some(max_iterations))?;
-    sizes.sort();
+fn part_1_inner(input: &str, max_iterations: usize) -> Option<usize> {
+    let (circuits, _, _) = solve(input, Some(max_iterations))?;
+    let mut sizes: Vec<usize> = circuits
+        .parent
+        .iter()
+        .enumerate()
+        .filter(|(i, p)| i == *p)
+        .map(|(i, _)| circuits.size[i])
+        .collect();
+    sizes.sort_unstable();
     Some(sizes.pop()? * sizes.pop()? * sizes.pop()?)
 }
 
@@ -24,71 +71,32 @@ pub fn part_2(input: &str) -> Option<u64> {
     Some(a.x * b.x)
 }
 
-fn solve(input: &str, max_iterations: Option<usize>) -> Option<(Vec<u64>, Point, Point)> {
+fn solve(input: &str, max_iterations: Option<usize>) -> Option<(DisjointSet, Point, Point)> {
     let junctions = parse(input);
-    let max_size = junctions.len() as u64;
     let mut distances = measure_distances(&junctions);
-
-    let mut circuits: HashMap<usize, usize> = HashMap::new();
-    let mut next_circuit = 1usize;
-    let mut sizes: Vec<u64> = Vec::new();
+    let mut circuits = DisjointSet::new(junctions.len());
 
     let mut a = 0usize;
     let mut b = 0usize;
     for _ in 0..max_iterations.unwrap_or(distances.len()) {
         (a, b, _) = distances.pop()?;
-
-        match (circuits.get(&a).copied(), circuits.get(&b).copied()) {
-            (None, None) => {
-                circuits.insert(a, next_circuit);
-                circuits.insert(b, next_circuit);
-
-                if sizes.len() <= next_circuit {
-                    sizes.resize(next_circuit + 1, 0);
-                }
-                sizes[next_circuit] = 2;
-
-                next_circuit += 1;
-            }
-            (Some(c), None) | (None, Some(c)) => {
-                circuits.insert(a, c);
-                circuits.insert(b, c);
-
-                sizes[c] += 1;
-                if sizes[c] >= max_size {
-                    break;
-                }
-            }
-            (Some(ca), Some(cb)) if ca != cb => {
-                let to_move: Vec<usize> = circuits
-                    .iter()
-                    .filter_map(|(j, c)| if *c == cb { Some(*j) } else { None })
-                    .collect();
-                for j in to_move {
-                    circuits.insert(j, ca);
-                }
-
-                sizes[ca] += sizes[cb];
-                sizes[cb] = 0;
-                if sizes[ca] >= max_size {
-                    break;
-                }
-            }
-            _ => {}
+        circuits.union(a, b);
+        if circuits.find_size(a) >= junctions.len() {
+            break;
         }
     }
 
-    Some((sizes, junctions[a], junctions[b]))
+    Some((circuits, junctions[a], junctions[b]))
 }
 
-fn measure_distances(junctions: &Vec<Point>) -> Vec<(usize, usize, u64)> {
+fn measure_distances(junctions: &[Point]) -> Vec<(usize, usize, u64)> {
     let mut distances = Vec::new();
     for (i, a) in junctions.iter().enumerate() {
         for (j, b) in junctions[(i + 1)..junctions.len()].iter().enumerate() {
             distances.push((i, i + 1 + j, distance(a, b)));
         }
     }
-    distances.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
+    distances.sort_by_key(|d| std::cmp::Reverse(d.2));
     distances
 }
 
